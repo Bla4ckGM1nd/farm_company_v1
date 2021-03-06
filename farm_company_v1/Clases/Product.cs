3fac736d300ccf7c8f4ea7aae43f3b53082e9119
unknown <stephagarcia02@gmail.com>
2021-03-06 00:10:52 -0500

    This is my first commit

---
+++ farm_company_v1/Clases/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace farm_company_v1.Clases
+{
+    public class Product
+    {
+        public string Name;// (e.g. potatoes, tomatoes... etc)
+        public float UnitPrice; //lb
+        public float UnitPerKilometer;
+        public float CropExtensionKms;
+        public float TaxPerUnitPercentage; //Tax per each produced crop unit.
+    }
+}
